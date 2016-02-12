72ea78c192d5b1bf49067da8f40552c707d2ce8b
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-02-12 14:13:04 -0500

    Update promisify.js

--- promisify.js
+++ promisify.js
@@ -12,39 +12,62 @@ function promisify(...functionsobjects) {
   }
   return(object[parts[parts.length - 1]])
  }
- for(let path of functionsobjects) {
-  let item = self[path] || getsetproperty(path)
-  if(typeof(item) == "function") {
-   let oldfunction = item
-   if(oldfunction.callbacks == null) {
-    oldfunction.callbacks = 2
-   }
-   let promise = function(...inputs) {
-    return(new Promise(function(resolve, reject) {
-     if(oldfunction.callbacks > 0) {
-      inputs.push(function() {
-       if(arguments.length == 1) {
-        resolve(arguments[0])
-       } else {
-        resolve(arguments)
-     } }) }
-     if(oldfunction.callbacks == 2) {
-      inputs.push(function(error) {
-       reject(error)
-     }) }
-     let newfunction = oldfunction
-     for(let input of inputs) {
-      newfunction = newfunction.bind(null, input)
-     }
-     newfunction()
-   })) }
-   if(oldfunction.callbacks > 0) {
-    if(path.includes(".")) {
-     getsetproperty(path, promise)
-    } else {
-     self[path] = promise
+ for(let item of functionsobjects) {
+  if(typeof(item) == "string") {
+   let path = item
+   item = self[path] || getsetproperty(path)
+   if(typeof(item) == "function") {
+    let oldfunction = item
+    oldfunction.callbacks = oldfunction.callbacks || 2
+    let promise = function(...inputs) {
+     let originalthis = this
+     return(new Promise(function(resolve, reject) {
+      if(oldfunction.callbacks > 0) {
+       inputs.push(function() {
+        if(arguments.length == 1) {
+         resolve(arguments[0])
+        } else {
+         resolve(arguments)
+      } }) }
+      if(oldfunction.callbacks == 2) {
+       inputs.push(function(error) {
+        reject(error)
+      }) }
+      oldfunction.apply(originalthis, inputs)
+    })) }
+    if(oldfunction.callbacks > 0) {
+     if(path.includes(".")) {
+      getsetproperty(path, promise)
+     } else {
+      self[path] = promise
+    } }
+   } else if(typeof(item) == "object") {
+    for(let property of item) {
+     promisify(path + "." + property.key)
    } }
-  } else if(typeof(item) == "object") {
-   for(let property of item) {
-    promisify(path + "." + property.key)
-} } } }
+  } else {
+   if(typeof(item) == "object") {
+    for(let property of item) {
+     item[property.key] = promisify(property.value)
+    }
+    return(item)
+   } else if(typeof(item) == "function") {
+    let oldfunction = item
+    oldfunction.callbacks = oldfunction.callbacks || 2
+    if(oldfunction.callbacks > 0) {
+     return(function(...inputs) {
+      let originalthis = this
+      return(new Promise(function(resolve, reject) {
+       if(oldfunction.callbacks > 0) {
+        inputs.push(function() {
+         if(arguments.length == 1) {
+          resolve(arguments[0])
+         } else {
+          resolve(arguments)
+       } }) }
+       if(oldfunction.callbacks == 2) {
+        inputs.push(function(error) {
+         reject(error)
+       }) }
+       oldfunction.apply(originalthis, inputs)
+})) }) } } } } }
